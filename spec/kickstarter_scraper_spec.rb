dacbb123a7d7f8cd9629920df275051abe30dd6f
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2020-01-07 16:01:04 +0000

    Automatically backed up by Learn

--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -31,4 +31,17 @@ def create_project_hash
 end
 end
 
-
+:projects => {
+  "My Great Project"  => {
+    :image_link => "Image Link",
+    :description => "Description",
+    :location => "Location",
+    :percent_funded => "Percent Funded"
+  },
+  "Another Great Project" => {
+    :image_link => "Image Link",
+    :description => "Description",
+    :location => "Location",
+    :percent_funded => "Percent Funded"
+  }
+}
